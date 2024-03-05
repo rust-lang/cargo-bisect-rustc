4a24b2d0c4558d1ee4074dbc8599550ec251365d
Eric Huss <eric@huss.org>
2024-03-05 13:15:13 -0800

    Fix build script to prevent change scanning.

--- build.rs
+++ build.rs
@@ -2,4 +2,6 @@ use std::env;
 
 fn main() {
     println!("cargo:rustc-env=HOST={}", env::var("TARGET").unwrap());
+    // Prevents cargo from scanning the whole directory for changes.
+    println!("cargo:rerun-if-changed=build.rs");
 }
